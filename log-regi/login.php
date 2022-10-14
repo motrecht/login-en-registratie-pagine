d2e741902fee5414f0a693368f41ecc2aa03e41a
motrecht <98494483+motrecht@users.noreply.github.com>
2022-10-14 14:07:43 +0200

    Add files via upload

---
+++ log-regi/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type= "text/css" href="Login.css">
+    <script src="https://kit.fontawesome.com/17a0720b93.js" crossorigin="anonymous"></script>
+    <title>Login Pagina</title>
+</head>
+<body>
+    
+    <div class="hero">
+        <h1>Login met je account</h1>
+        <div class="signup-box">
+                 <div class="left-box">
+                        <a href="" class="fb-link"><i class="fa-brands fa-facebook"></i> verbind Facebook</a>
+                        <a href="" class="tw-link"><i class="fa-brands fa-twitter"></i> verbind Twitter</a>
+                        <a href="" class="ins-link"><i class="fa-brands fa-instagram"></i> verbind Instagram</a>
+                    </div>
+        
+
+                    <form>
+                    <div class="right-box">
+                        <input type="email" placeholder="email adres" required class="input-box"> 
+                        <input type="password" placeholder="Wachtwoord" required class="input-box">
+                        <button type="submit">Login<span>&#x27f6;</span></button>
+                        </form>
+                    </div>
+                
+        </div>
+        <p class="signup-msg">Heb je geen account? <a href="#">Maak nu een account aan.</a></p>
+    </div>
+
+
+
+
+</body>
+</html>
---
+++ log-regi/register.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type= "text/css" href="regis.css">
+    <script src="https://kit.fontawesome.com/17a0720b93.js" crossorigin="anonymous"></script>
+    <title>register pagina</title>
+</head>
+<body>
+    
+    <div class="hero">
+        <h1>Registreer je accout</h1>
+        <div class="signup-box">
+            <div class="left-box">
+                <form>
+                    <input type="text" placeholder="je naams" required class="input-box">
+                    <input type="text" placeholder="je achternaam" require class="input-box">
+                    <input type="email" placeholder="je mailadres" required class="input-box">
+                    <input type="password" placeholder="Maak een wachtwoord aan" required >
+                    <input type="checkbox" id="voorwaarden">
+                    <label for="voorwaarden">ik accrepteer de voorwaarden.</label>
+                    <button type="submit">Meld aan<span>&#x27f6;</span></button>
+                </form>
+                </div>
+                    <div class="right-box">
+                        <a href="" class="fb-link"><i class="fa-brands fa-facebook"></i> Connect whit Facebook</a>
+                        <a href="" class="tw-link"><i class="fa-brands fa-twitter"></i> Connect whit Twitter</a>
+                        <a href="" class="ins-link"><i class="fa-brands fa-instagram"></i> Connect whit Instagram</a>
+                    </div>
+        </div>
+        <p class="login-msg">Heb je al een account? <a href="#">Login nu</a></p>
+    </div>
+
+
+
+
+</body>
+</html>
